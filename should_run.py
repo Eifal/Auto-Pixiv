db76c91ebc66819d0df23e00d363250680006160
Eifvl 運命 <syahrifal7@gmail.com>
2024-06-30 00:02:47 +0700

    Update should_run.py

--- should_run.py
+++ should_run.py
@@ -1,7 +1,12 @@
 import random
 import os
 
-should_run = random.random() < 0.1
+event_name = os.environ.get('GITHUB_EVENT_NAME')
 
+if event_name == 'workflow_dispatch':
+    should_run = True
+else:
+    should_run = random.random() < 0.1
+    
 with open(os.environ['GITHUB_ENV'], 'a') as env_file:
     env_file.write(f"SHOULD_RUN={should_run}\n")
