fe70e5a303e47c7b6a4af4d410210f5479f704f2
Eifvl 運命 <syahrifal7@gmail.com>
2024-09-29 00:10:57 +0700

    up to 50%

--- should_run.py
+++ should_run.py
@@ -6,7 +6,7 @@ event_name = os.environ.get('GITHUB_EVENT_NAME')
 if event_name == 'workflow_dispatch':
     should_run = True
 else:
-    should_run = random.random() < 0.3
+    should_run = random.random() < 0.5
     
 with open(os.environ['GITHUB_ENV'], 'a') as env_file:
     env_file.write(f"SHOULD_RUN={should_run}\n")
